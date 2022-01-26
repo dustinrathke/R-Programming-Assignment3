cee2f386ee819b76afbfc5c8dd7333e5cfdb5f1c
Dustin Rathke <dustinrathke24@gmail.com>
2022-01-26 08:21:47 -0600

    replacing wrong function

--- rankall.R
+++ rankall.R
@@ -1,26 +1,35 @@
-rankhospital <- function(state, outcome, num = "best") {
+rankall <- function(outcome, num = "best") {
   ## Read data
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character",na.strings="Not Available")
   
-  ## Checking that inputs are valid
+  ## Check that inputs are valid
   validOutcome = c("heart attack","heart failure","pneumonia")
   if (!outcome %in% validOutcome) { stop("invalid outcome")}
   
-  validState = unique(data[,7])
-  if (!state %in% validState) stop("invalid state")
+  validState = sort(unique(data[,7]))
+  ##if (!state %in% validState) stop("invalid state")
   
-  ## convert outcome name
+  ## convert outcome
   fullColName <- c("Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia")
   colName <- fullColName[match(outcome,validOutcome)]
   
-  ## Gives hospital name with rank
-  data.state <- data[data$State==state,]
+  ## Find rank for hospital by state
+  hospital<-character(0)
   
-  # Order the data
-  sorted.data.state <- data.state[order(as.numeric(data.state[[colName]]),data.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
+  for (i in seq_along(validState)) {
+    ## Return hospital name in that state with the given rank 30-day death rate
+    data.state <- data[data$State==validState[i],]
+    
+    # order data
+    sorted.data.state <- data.state[order(as.numeric(data.state[[colName]]),data.state[["Hospital.Name"]],decreasing=FALSE,na.last=NA), ]
+    
+    this.num = num
+    if (this.num=="best") this.num = 1
+    if (this.num=='worst') this.num = nrow(sorted.data.state)
+    
+    hospital[i] <- sorted.data.state[this.num,"Hospital.Name"]
+  }
   
-  if (num=="best") num = 1
-  if (num=='worst') num = nrow(sorted.data.state)
-  
-  sorted.data.state[num,"Hospital.Name"]
+  ## Return a data frame with the hospital names and the (abbreviated) state name
+  data.frame(hospital=hospital,state=validState,row.names=validState)
 }
